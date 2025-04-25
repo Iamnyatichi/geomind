6aa9cc1109617970535b23c75f029fb1f3bf12a5
THERESA NYATICHI MONARI <tresamonari@gmail.com>
2025-04-25 16:27:01 +0300

    Updates

--- geomind/geomind.py
+++ geomind/geomind.py
@@ -1,5 +1,6 @@
 """Main module."""
 
+import os
 import ipyleaflet
 
 
@@ -7,8 +8,46 @@ class Map(ipyleaflet.Map):
     def __init__(self, center=[20, 0], zoom=2, height="600px", **kwargs):
         super().__init__(center=center, zoom=zoom, **kwargs)
         self.layout.height = height
+        self.scroll_wheel_zoom = True
 
     def add_basemap(self, basemap="OpenTopoMap"):
         url = eval(f"ipyleaflet.basemaps.{basemap}").build_url()
         layer = ipyleaflet.TileLayer(url=url, name=basemap)
         self.add_layer(layer)
+
+    def add_geojson(self, data, zoom_to_layer=True, hover_style=None, **kwargs):
+
+        import geopandas as gpd
+
+        if hover_style is None:
+            hover_style = {"color": "yellow", "fillOpacity": 0.2}
+
+        if isinstance(data, str):
+            gdf = gpd.read_file(data)
+            geojson = gdf.__geo_interface__
+        elif isinstance(data, dict):
+            geojson = data
+            gdf = gpd.GeoDataFrame.from_features(geojson["features"])
+
+        layer = ipyleaflet.GeoJSON(data=geojson, hover_style=hover_style, **kwargs)
+        self.add_layer(layer)
+
+        if zoom_to_layer:
+            bounds = gdf.total_bounds
+            self.fit_bounds([[bounds[1], bounds[0]], [bounds[3], bounds[2]]])
+
+    def add_shp(self, data, **kwargs):
+        import geopandas as gpd
+
+        gdf = gpd.read_file(data)
+        gdf = gdf.to_crs(epsg=4326)
+        geojson = gdf.__geo_interface__
+        self.add_geojson(geojson, **kwargs)
+
+    def add_gdf(self, gdf, **kwargs):
+        gdf = gdf.to_crs(epsg=4326)
+        geojson = gdf.__geo_interface__
+        self.add_geojson(geojson, **kwargs)
+
+    def add_vector(self, data, **kwargs):
+        import geopandas as gpd
